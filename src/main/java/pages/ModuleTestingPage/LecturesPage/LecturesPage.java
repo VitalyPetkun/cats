e8128324fa33d9bfd4bcd3ddf2798afaebc27b55
Vitaly Petkun <p.u.d.i.n.g.69.v@gmail.com>
2022-06-05 21:55:09 +0300

    Add file LecturesPage.java

---
+++ src/main/java/pages/ModuleTestingPage/LecturesPage/LecturesPage.java
@@ -0,0 +1,27 @@
+package pages.ModuleTestingPage.LecturesPage;
+
+import elements.Button;
+import elements.Iframe;
+import elements.Link;
+import framework.BaseForm;
+import framework.browser.Browser;
+import org.openqa.selenium.By;
+
+public class LecturesPage extends BaseForm {
+
+    public LecturesPage() {
+        super(new Link(By.xpath("//div[@class='labs-component__body']"), "Lectures page uniq element"),
+                "Lectures page");
+    }
+
+    public void switchToIframeLectures() {
+        new Iframe(By.xpath("//iframe"), "Lectures").switchToIframe();
+    }
+
+    public void clickVisitStatistics() {
+        this.switchToIframeLectures();
+        new Button(By.xpath("//span[contains(text(),'Посещение лекций')]/parent::span/parent::button"),
+                "Visit statistics").click();
+        Browser.switchToDefaultContent();
+    }
+}
